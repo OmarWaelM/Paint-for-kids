50cb088f8508f2bd4733ae85df4a24253bb8aab0
tokamohamed03 <toka090903@gmail.com>
2024-05-02 04:02:35 +0300

    edit

--- SwitchToPlayModeAction.h
+++ SwitchToPlayModeAction.h
@@ -1,1 +1,12 @@
+#ifndef SwitchToPlayModeAction_H
+#define SwitchToPlayModeAction_H
+
 #pragma once
+#include "ApplicationManager.h"
+#include "Actions/Action.h"
+
+class SwitchToPlayModeAction : public Action
+{
+private:
+
+};
