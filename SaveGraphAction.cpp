5d6cde7cdcd4b76abf122e06e35a94b4c0378533
tokamohamed03 <toka090903@gmail.com>
2024-05-03 19:39:03 +0300

    edit

--- SaveGraphAction.cpp
+++ SaveGraphAction.cpp
@@ -15,87 +15,39 @@ void SaveGraphAction::ReadActionParameters()
 void SaveGraphAction::Execute()
 {
 	Output* pOut = pManager->GetOutput();
-	int CrntDrawColour;
-	int CrntFillColour;
-	string draw, fill;
+	string CrntDrawColour;
+	string CrntFillColour;
 
 	if (pOut->getCrntDrawColor() == BLACK)
-		CrntDrawColour = BLACK_COLOUR;
+		CrntDrawColour = "BLACK";
 	else if (pOut->getCrntDrawColor() == YELLOW)
-		CrntDrawColour = YELLOW_COLOUR;
+		CrntDrawColour = "YELLOW";
 	else if (pOut->getCrntDrawColor() == ORANGE)
-		CrntDrawColour = ORANGE_COLOUR;
+		CrntDrawColour = "ORANGE";
 	else if (pOut->getCrntDrawColor() == RED)
-		CrntDrawColour = RED_COLOUR;
+		CrntDrawColour = "RED";
 	else if (pOut->getCrntDrawColor() == GREEN)
-		CrntDrawColour = GREEN_COLOUR;
+		CrntDrawColour = "GREEN";
 	else if (pOut->getCrntDrawColor() == BLUE)
-		CrntDrawColour = BLUE_COLOUR;
+		CrntDrawColour = "BLUE";
 
 	if (pOut->getCrntFillColor() == BLACK)
-		CrntFillColour = BLACK_COLOUR;
+		CrntFillColour = "BLACK";
 	else if (pOut->getCrntFillColor() == YELLOW)
-		CrntFillColour = YELLOW_COLOUR;
+		CrntFillColour = "YELLOW";
 	else if (pOut->getCrntFillColor() == ORANGE)
-		CrntFillColour = ORANGE_COLOUR;
+		CrntFillColour = "ORANGE";
 	else if (pOut->getCrntFillColor() == RED)
-		CrntFillColour = RED_COLOUR;
+		CrntFillColour = "RED";
 	else if (pOut->getCrntFillColor() == GREEN)
-		CrntFillColour = GREEN_COLOUR;
+		CrntFillColour = "GREEN";
 	else if (pOut->getCrntFillColor() == BLUE)
-		CrntFillColour = BLUE_COLOUR;
-	else CrntFillColour = NO_FILL_COLOUR;
+		CrntFillColour = "BLUE";
+	else CrntFillColour = "NO_FILL";
 
 	ofstream OutputFile;
 	OutputFile.open(FileName + ".txt", ios::out);
-	
-	switch (CrntDrawColour)
-	{
-	case BLACK_COLOUR:
-		draw = 'BLACK';
-		break;
-	case YELLOW_COLOUR:
-		draw = 'YELLOW';
-		break;
-	case ORANGE_COLOUR:
-		draw = 'ORANGE';
-		break;
-	case RED_COLOUR:
-		draw = 'RED';
-		break;
-	case GREEN_COLOUR:
-		draw = 'GREEN';
-		break;
-	case BLUE_COLOUR:
-		draw = 'BLUE';
-		break;
-	}
-
-	switch (CrntFillColour)
-	{
-	case BLACK_COLOUR:
-		OutputFile << "BLACK" << '\n';
-		break;
-	case YELLOW_COLOUR:
-		OutputFile << "YELLOW" << '\n';
-		break;
-	case ORANGE_COLOUR:
-		OutputFile << "ORANGE" << '\n';
-		break;
-	case RED_COLOUR:
-		OutputFile << "RED" << '\n';
-		break;
-	case GREEN_COLOUR:
-		OutputFile << "GREEN" << '\n';
-		break;
-	case BLUE_COLOUR:
-		OutputFile << "BLUE" << '\n';
-		break;
-	case NO_FILL_COLOUR:
-		OutputFile << "NO_FILL" << '\n';
-		break;
-	}
-
+	OutputFile << CrntDrawColour << "	" << CrntFillColour << endl;
 	pManager->SaveAllFigures(OutputFile);
 	OutputFile.close();
 	
