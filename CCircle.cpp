12f6dd3da865d55d492b0d144cba566c5c7d93db
OmarWaelM <omwamo05@gmail.com>
2024-05-05 15:48:44 +0300

    fteer

--- CCircle.cpp
+++ CCircle.cpp
@@ -48,7 +48,7 @@ void CCircle::Move(Point P)
 {
 	int dist_x = P.x - Centre.x;
 	int dist_y = P.y - Centre.y;
-	Centre.y = P.x; 
+	Centre.x = P.x; 
 	Centre.y = P.y;
 	Radius.x = Radius.x + dist_x;
 	Radius.y = Radius.y + dist_y;
