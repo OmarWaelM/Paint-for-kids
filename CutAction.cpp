b576a34c4600f35adfd65056c79d835edef0ab17
arwaibrahiim <166602987+arwaibrahiim@users.noreply.github.com>
2024-05-06 09:41:51 +0300

    CutAction Comments

--- CutAction.cpp
+++ CutAction.cpp
@@ -10,21 +10,24 @@ void CutAction::ReadActionParameters()
 
 void CutAction::Execute()
 {
-	pOut = pManager->GetOutput();
-	pOut->PrintMessage("Cut tool selected");
+	pOut = pManager->GetOutput();						//initializing pOut pointer
+	pOut->PrintMessage("Cut Tool Selected");			//print message
 
+	//checks if only one figure is selected
 	if (pManager->GetSelectedCount() == 1)
 	{
-		SelectedFig = pManager->GetSelected(0);
-		pManager->SetClipboard(SelectedFig);
-		SelectedFig->SetCut(true);
+		SelectedFig = pManager->GetSelected(0);			//if yes, set the SelectedFig pointer to this figure
+		pManager->SetClipboard(SelectedFig);			//sets the clipboard to this figure
+		SelectedFig->SetCut(true);						//sets cut status to true (the figure is cut)
 	}
+	//if no figure is selected
 	else if (pManager->GetSelectedCount() == 0)
 	{
-		pOut->PrintMessage("No items are selected");
+		pOut->PrintMessage("No Items are Selected");	//print message
 	}
+	//if more than one shape is selected
 	else
 	{
-		pOut->PrintMessage("Select ONLY ONE Figure");
+		pOut->PrintMessage("Select ONLY ONE Figure");	//print message
 	}
 }
