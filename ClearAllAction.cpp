4c6886248d76e77310f9b6952f448dcb8595b9ea
arwaibrahiim <166602987+arwaibrahiim@users.noreply.github.com>
2024-05-06 09:10:26 +0300

    ClearAll Comments

--- ClearAllAction.cpp
+++ ClearAllAction.cpp
@@ -12,21 +12,25 @@ void ClearAllAction::ReadActionParameters()
 void ClearAllAction::Execute()
 {
 	pOut = pManager->GetOutput();
-	pOut->ClearDrawArea();
-	pOut->ClearStatusBar();
-	pManager->SetClipboard(NULL);
+	pOut->ClearDrawArea(); //clears the drawing area
+	pOut->ClearStatusBar(); //clears the status bar
+	pManager->SetClipboard(NULL); //empties the clipboard
 
+	//clears the selected figure list items
 	for (int i = pManager->GetSelectedCount() - 1; i >= 0; i--)
 	{
 		Fig = pManager->GetSelected(i);
 		pManager->DeleteSelected(i, Fig);
 	}
 
+	//clears the figure list items
 	for (int i = pManager->GetFigCount() - 1; i >= 0; i--)
 	{
 		Fig = pManager->GetFigListItem(i);
 		pManager->Delete_Figure(Fig);
 	}
+
+	//resets the number of figures
 	CFigure::ChngNumberOfFigures(0);
 }
 
